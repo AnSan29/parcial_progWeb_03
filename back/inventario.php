7dde3b3d3ebbaedb467baf96ac8bed6834c54cea
Kelmin Miranda <kelminemh@hotmail.com>
2025-03-19 17:28:04 -0500

    Validacion y seleccion por codigo de producto

--- back/inventario.php
+++ back/inventario.php
@@ -1,6 +1,30 @@
 <?php 
 session_start();
-
-$_SESSION['productos']= [
-    'codigo' => 0
-];
+$_SESSION[$productos] = array(
+    123456 => array(
+    "nombre_producto" => "grifos",
+    "unidad" => "unidades",
+    "existencia" => 1000
+    ),
+    234567 => array(
+    "nombre_producto" => " martillos",
+    "unidad" => "unidades",
+    "existencia" => 40
+    ),
+    345678 => array(
+    "nombre_producto" => "cemento",
+    "unidad" => "bolsas",
+    "existencia" => 1000
+    ),
+    456790 => array(
+    "nombre_producto" => "bombillas",
+    "unidad" => "unidades",
+    "existencia" => 1000
+    ),
+    5678901 => array(
+    "nombre_producto" => "Cables eléctricos",
+    "unidad" => "metros",
+    "existencia" => 1000
+    )
+    );
+   
