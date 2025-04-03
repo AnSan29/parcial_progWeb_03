cef1052a5fbe754e34a5a8c7755111c6d5cc3dc1
Kelmin Miranda <kelminemh@hotmail.com>
2025-04-03 12:23:06 -0500

    Formularios listos para aplicar estilos

--- front/crear_pedido.php
+++ front/crear_pedido.php
@@ -2,12 +2,83 @@
 session_start();
 if (!isset($_SESSION['codigo'])) {
     echo "<script> 
-    alert('Digite primero un codigo')
-    setTimeout(function() {
-    window.location.href = 'index.php';
-}, 1500)
-    </script>";
-} else {
-echo $_SESSION['codigo'];
+    let permiso_entrada = confirm('Digite primero un codigo')
 
+    if (permiso_entrada) {
+    window.location.href = 'index.php';
+    } else {
+    window.location.href = 'index.php';
 }
+    </script>";
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Crear Pedido</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
+        <link rel="stylesheet" href="css/crear-pedido.css">
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+        <a href="index.php">Volver</a>
+
+    </header>
+    <main>
+
+        <form class="row g-3" action="../back/nuevo_pedido.php" method="post">
+            <div class="col-md-6">
+                <p>Cliente</p>
+                <input class="form-control" type="text" name="nombreCliente" required>
+            </div>
+            <div class="col-md-6">
+                <p>Codigo de producto</p>
+                <input class="form-control" type="text" value="<?= $_SESSION['codigo'] ?>" disabled>
+            </div>
+            <div class="col-md-6">
+                <p>Cantidad</p>
+                <input class="form-control" type="number" name="cantidad" required min="1" max="<?= $_SESSION['producto']['existencia'] ?>">
+            </div>
+            <div class="col-md-6">
+
+                <p>Valor</p>
+                <input class="form-control" type="number" name="valor" required>
+            </div>
+
+            <button name="agregarPedido">Agregar</button>
+            
+        </form>
+
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
